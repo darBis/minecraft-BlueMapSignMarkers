e87717fa20eaacbd6103dd2c1658cd43dc873a7d
Martin Darebný <martin.darebny@gmail.com>
2025-08-30 15:58:10 +0200

    improved sign text rendering, adjusted icon positioning

--- src/main/java/dev/kugge/signmarkers/watcher/SignWatcher.java
+++ src/main/java/dev/kugge/signmarkers/watcher/SignWatcher.java
@@ -18,6 +18,8 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SignWatcher implements Listener {
     @EventHandler
@@ -46,13 +48,23 @@ public class SignWatcher implements Listener {
             return;
         }
 
-        // TODO: improve text handling
+        var lines = new ArrayList<String>(2);
         Component clabel1 = event.line(1);
-        if (clabel1 == Component.empty() || clabel1 == null) return;
-
+        if (clabel1 != null && clabel1 != Component.empty()){
+            lines.add(LegacyComponentSerializer.legacySection().serialize(clabel1));
+        }
         Component clabel2 = event.line(2);
-        String label = LegacyComponentSerializer.legacySection().serialize(clabel1) + "\n"
-                + LegacyComponentSerializer.legacySection().serialize(clabel2);
+        if (clabel2 != null && clabel2 != Component.empty()){
+            lines.add(LegacyComponentSerializer.legacySection().serialize(clabel2));
+        }
+        if (lines.size() == 0) {
+            event.line(1, Component.text("<put text here!>"));
+            event.getPlayer().sendMessage(Component.text("SignMarkers: Missing marker text!").color(NamedTextColor.RED));
+            return;
+        }
+
+        String label = String.join(" ", lines);
+        String detail = getDetailHtml(iconName, lines);
 
         Block block = event.getBlock();
 
@@ -60,7 +72,7 @@ public class SignWatcher implements Listener {
 
         // make marker id from original block position without offset, to correctly match marker when sign gets destroyed
         String id = SignMarkers.MARKER_PREFIX + block.getX() + "-" + block.getY() + "-" + block.getZ();
-        POIMarker marker = POIMarker.builder().label(label).position(markerPos).icon(ii.iconAddress, ii.anchor).maxDistance(100000).build();
+        POIMarker marker = POIMarker.builder().label(label).detail(detail).position(markerPos).icon(ii.iconAddress, ii.anchor).maxDistance(100000).build();
         SignMarkers.markerSet.get(block.getWorld()).put(id, marker);
 
         // Delete [map] and icon lines
@@ -68,6 +80,19 @@ public class SignWatcher implements Listener {
         event.line(3, Component.empty());
 
         event.getPlayer().sendMessage(Component.text("Created '" + iconName + "' marker"));
+    }
+
+    private static @NotNull String getDetailHtml(String iconName, List<String> lines) {
+        var detailTempltate =
+                " <div style=\"display:flex; align-items:center; gap:8px;\">\n" +
+                "    <img src=\"./markers/{icon}.png\" style=\"flex:0 0 auto;\">\n" +
+                "    <div style=\"display:flex; flex-direction:column;\">\n" +
+                "      {lines}\n" +
+                "    </div>\n" +
+                "  </div>";
+
+        String detail = detailTempltate.replace("{icon}", iconName).replace("{lines}",String.join("\n", lines.stream().map(s-> "<div style=\"white-space:nowrap\">" + s + "</div>").toList()));
+        return detail;
     }
 
     private static @NotNull Vector3d getMarkerPosition(Block block) {
@@ -79,9 +104,9 @@ public class SignWatcher implements Listener {
         boolean isWallSign = block.getBlockData().getMaterial().name().contains("WALL_SIGN");
         boolean isHangingSign = block.getBlockData().getMaterial().name().contains("HANGING_SIGN");
         if (isWallSign) {
-            yPos = yPos + 1;
+            yPos = yPos + .5;
         } else if (isHangingSign) {
-            yPos = yPos + 0.75;
+            yPos = yPos + 0.3;
         } else {
             yPos = yPos + 0.8; // base offset for standalone sign
         }
@@ -95,16 +120,18 @@ public class SignWatcher implements Listener {
             switch (facing) {
                 case NORTH: // Sign facing north, attached to south face
                     xPos = xPos + 0.5;
-                    zPos = zPos + 1.0;
+                    zPos = zPos + 1.0-.1;
                     break;
                 case SOUTH: // Sign facing south, attached to north face
                     xPos = xPos + 0.5;
+                    zPos = zPos + .1;
                     break;
                 case EAST:  // Sign facing east, attached to west face
+                    xPos = xPos + .1;
                     zPos = zPos + 0.5;
                     break;
                 case WEST:  // Sign facing west, attached to east face
-                    xPos = xPos + 1.0;
+                    xPos = xPos + 1.0-.1;
                     zPos = zPos + 0.5;
                     break;
                 default:
